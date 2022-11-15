8893f6bb61b168de3a6eba99daf7e8e4a332fabf
sidheshhiphop <91182522+sidheshhiphop@users.noreply.github.com>
2022-11-15 15:07:16 +0530

    Add files via upload

---
+++ wallrappers-react/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <img src={logo} className="App-logo" alt="logo" />
+    <h1>WALL RAPPERS</h1>
+    </div>
+  );
+}
+
+export default App;
