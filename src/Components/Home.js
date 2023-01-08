8c95320119ce80087a2ac3042d3651c0f0dfd22c
sidhesh <sidheshravichandran@gmail.com>
2023-01-08 21:07:36 +0530

    home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -19,10 +19,10 @@ function Home() {
         <section class="text-black body-font lg:pt-20">
       <div class="container px-5 pt-32 mx-auto lg:px-4 lg:py-4">
         <div class="flex flex-col w-full mb-2 text-left md:text-center ">
-          <h1 class="mb-2 text-6xl font-bold tracking-tighter text-black lg:text-8xl md:text-7xl">
-            <span>Hip-hop is the streets</span>
+          <h1 class="mb-2 text-2xl font-bold tracking-tighter text-black lg:text-8xl md:text-7xl">
+            <span>Hip-hop is the streets, </span>
             <br class="hidden lg:block"></br>
-            Good music = good vibes
+            Good music = good vibes!
           </h1>
           <br></br>
           <p class="mx-auto  text-xl font-normal leading-relaxed text-gray-600 dark:text-gray-300 lg:w-2/3">
